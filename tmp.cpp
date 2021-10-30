432da1940d5da21f1f2ff64d7067ff53e9f7d94a
ishankhan7572 <86646962+ishankhan7572@users.noreply.github.com>
2021-10-30 09:50:05 +0530

    Create tmp.cpp

---
+++ tmp.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int lli;
+typedef long long ll;
+typedef vector< int > vi;
+typedef vector< vi > vvi;
+typedef pair< int,int > ii;
+#define rep(i, n) for(int i = 0; i < n; i++)
+#define rrep(i,n) for(int i = n-1; i >= 0; i--)
+
+int main()
+{
+    
+ios_base::sync_with_stdio(false); 
+cin.tie(NULL);
+ll  t,n;
+cin>>t;
+while(t--)
+{
+    
+    
+    
+}
+}
+
+
+
+
+
+
