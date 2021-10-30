8deaee94c6b3f3a5cab48b7a2b8345aabbb0fc05
ishankhan7572 <86646962+ishankhan7572@users.noreply.github.com>
2021-10-30 10:32:07 +0530

    Update tmp.cpp

--- tmp.cpp
+++ tmp.cpp
@@ -8,7 +8,7 @@ typedef vector< vi > vvi;
 typedef pair< int,int > ii;
 #define rep(i, n) for(int i = 0; i < n; i++)
 #define rrep(i,n) for(int i = n-1; i >= 0; i--)
-
+ll ceil_div(ll a,ll b){  return (a%b==0)? a/b :a/b+1 ;}
 int main()
 {
     
